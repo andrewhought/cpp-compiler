0f0b630a511a3c4a86a5b69bdcac7c82c31e5c24
andrewhought <80929614+boolin9@users.noreply.github.com>
2024-06-02 16:04:33 -0700

    added and modified test scripts

--- test2.sh
+++ test2.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 let count=0;
-for f in $(ls ./tests/lexer-tests/*.txt); do
-	./a.out <$f > ./tests/lexer-tests/`basename $f .txt`.output;
+for f in $(ls ./tests/parser-tests/*.txt); do
+	./a.out <$f > ./tests/parser-tests/`basename $f .txt`.output;
 done;
 
-for f in $(ls ./tests/lexer-tests/*.output); do
-	diff -Bw $f  ./tests/lexer-tests/`basename $f .output`.txt.expected > ./tests/lexer-tests/`basename $f .output`.diff;
+for f in $(ls ./tests/parser-tests/*.output); do
+	diff -Bw $f  ./tests/parser-tests/`basename $f .output`.txt.expected > ./tests/parser-tests/`basename $f .output`.diff;
 done
 
-for f in $(ls ./tests/lexer-tests/*.diff); do
+for f in $(ls ./tests/parser-tests/*.diff); do
 	echo -e "\n========================================================";
 	echo "FILE:" `basename $f .output`;
 	echo "========================================================";
 	if [ -s $f ]; then
-		cat ./tests/lexer-tests/`basename $f .diff`.txt;
+		cat ./tests/parser-tests/`basename $f .diff`.txt;
 		echo "--------------------------------------------------------";
 		cat $f
 		echo "========================================================";
@@ -27,5 +27,5 @@ done
 
 echo -e "\nTESTS PASSED:" $count"/12\n";
 
-rm tests/lexer-tests/*.output
-rm tests/lexer-tests/*.diff
+rm tests/parser-tests/*.output
+rm tests/parser-tests/*.diff
